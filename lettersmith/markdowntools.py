87b3ab634213070b69f1031cc33351d8f3feffe5
Brandon Hall <brandon@tacticaltypos.net>
2023-04-09 14:42:20 -0700

    Replace commonmark with marko
    Includes footnotes extension

--- lettersmith/markdowntools.py
+++ lettersmith/markdowntools.py
@@ -1,9 +1,9 @@
-from commonmark import commonmark
+from marko import Markdown
 from lettersmith.html import strip_html
 from lettersmith import docs as Docs
 from lettersmith.func import compose
 
 
-markdown = commonmark
+markdown = Markdown(extensions=['footnote'])
 strip_markdown = compose(strip_html, markdown)
 content = Docs.renderer(markdown)
